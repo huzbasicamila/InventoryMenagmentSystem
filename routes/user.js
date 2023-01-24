7a8c5be92f7fd90d83928ee5f2476a602ae8d690
Amila Huzbasic <92604250+huzbasicamila@users.noreply.github.com>
2023-01-24 04:00:24 +0100

    RADII

--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connection = require('../connection');
 const { query } = require('express');
-const express= require('express');
-const connection= require('../connection');
-const router=express.Router();
+const { restart } = require('nodemon');
+const router = express.Router();
 
-router.post('/signup', (req,res)=>{
-    let user= req.body;
-    query = "select email, password, role, status from user where email=?"
-    connection.query(query,[user.email],(err,results)=>{
-        if(!err){
-            if(results.lenght<=0) {
-                query="insert into user(name, contactNumber, email, password, status, role) values (?,?,?,?,'false','user')";
-                connection.query(query, [user.name,user.contactNumber,user.email,user.password],(err,results)=>{
-                    if(!err){
-                        return res.status(200).json({message:"Usješno registrovan"});
-                    }
-                    else {
-                        return res.status(500).json(err);
-                    }
-                })
-            } 
-            else{
-                return res.status(400).json({message: "Email već postoji."});
-            }
-        } 
-        else{
+router.use(bodyParser.json());
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+router.post('/signup', async (req, res) => {
+    try {
+        let user = req.body;
+        let query = "select email, password, role, status from user where email=?";
+        let results = await connection.query(query, [user.email]);
+        if (results.length <= 0) {
+            //query = "insert into user(name, contactNumber, email, password, status, role) values (?,?,?,?,'false','user')";
+            //connection.query(query, [user.name, user.contactNumber, user.email, user.password]);
+            //return res.status(200).json({ message: "Successfully Registered" });
+            return res.status(400).json({ message: "Email već postoji." });
+        } else {
+            query = "insert into user(name, contactNumber, email, password, status, role) values (?,?,?,?,'false','user')";
+            connection.query(query, [user.name, user.contactNumber, user.email, user.password]);
+            return res.status(200).json({ message: "Successfully Registered" });
+            //return res.status(400).json({ message: "Email već postoji." });
+        }
+    } catch (err) {
+        console.log(err);
         return res.status(500).json(err);
     }
-})
-    
-})
+});
 
-module.exports= router;
+router.post('/login', (req, res) => {
+    const user = req.body;
+    let query = "select email, password, role, status from user where email=?";
+    try{ 
+        
+        connection.query(query, [user.email], (err, results) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+            if (results.length <= 0 || results[0].password != user.password) {
+                return res.status(401).json({ message: "Netačan username ili password" });
+            }
+            else if (results[0].status === 'false') {
+                return res.status(401).json({ message: "Pricekajte da Admin odobri" });
+            }
+            else if (results[0].password == user.password) {
+                const response = { email: results[0].email, role: results[0].role }
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' })
+                res.status(200).json({ token: accessToken });
+            }
+            else {
+                return res.status(400).json({ message: "Nešto nije uredu. Pokušajte ponovo." });
+            }
+        })
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+});
+module.exports = router;
--- table.sql
+++ table.sql
@@ -1,7 +1,7 @@
 create table user(
     id int primary key AUTO_INCREMENT,
     name varchar(250),
-    contactNumber varchar(20),
+    contactNumber varchar(20), 
     email varchar(50),
     password varchar(250),
     status varchar(20),
