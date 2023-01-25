e36be0d2c569d22905f30a3d5dfe78715f865512
Amila Huzbasic <92604250+huzbasicamila@users.noreply.github.com>
2023-01-25 06:27:27 +0100

    sirovine

--- table.sql
+++ table.sql
@@ -11,3 +11,8 @@ create table user(
 
 insert into user(name,contactNumber, email, password,status,role) values ('Admin', '061234567', 'admin@gmail.com', 'admin', 'true', 'admin');
 
+create table sirovine{
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    primary key(id)
+};
